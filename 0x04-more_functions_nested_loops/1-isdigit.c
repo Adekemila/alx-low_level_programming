539ce91220f86eece9f7a20e46ace931ee678537
Adekemila <Adebayoov.18@student.funaab.edu.ng>
2022-12-16 02:09:32 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isdigit - function that verifies if a character is a digit or not
- * @c: tested character
- * Return: returns 1 if it is uppercase , 0 if not
+ *_isdigit - function that verifies if a character is a digit or not
+ *@c: tested character
+ *Return: returns 1 if it is uppercase , 0 if not
  */
 
 int _isdigit(int c)
@@ -12,4 +12,5 @@ return (1);
 
 return (0);
 }
+
 
