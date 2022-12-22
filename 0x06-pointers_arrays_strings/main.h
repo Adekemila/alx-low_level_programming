b50af40f17f3a70729d6e5864a31615ec56bd11f
Adekemila <Adebayoov.18@student.funaab.edu.ng>
2022-12-22 03:22:27 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *desk, char *src);
+char *_strncat(char *desk, char *src, int n);
+char *_strncpy(char *desk, cahr *src, int n);
+int _strcnp(char *s1, char *s2);
+void reverse_array(int *a, int b);
+char *string_topper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+int _putchar(char c);
+
+#endif 
